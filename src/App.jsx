7986f701666d25cb9c9d140e4921c8d8615d94f3
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-01-06 17:55:08 +0200

    Refactor the app architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,88 @@
-import Game from './components/Game.jsx';
+import { useState, useEffect } from 'react';
+import Info from './components/Info.jsx';
+import Board from './components/Board.jsx';
+import ResultModal from './components/ResultModal.jsx';
+import shuffleArray from './utils/shuffleArray.js';
+import fetchUniqueImageSources from './utils/fetchImageSources.js';
 
 function App() {
+  const [images, setImages] = useState(null);
+  const [memory, setMemory] = useState([]);
+  const [result, setResult] = useState({ highScore: 0, info: null });
+  const [lastFocusedCard, setLastFocusedCard] = useState(null);
+
+  useEffect(() => {
+    // Extra count added on fetch, so keep the count up to 32 to avoid exceeding 50 (API limit)
+    fetchUniqueImageSources(18)
+      .then((sources) => setImages(sources.map((src, id) => ({ id, src }))))
+      .catch((e) => setImages(e));
+  }, []);
+
+  useEffect(() => {
+    if (lastFocusedCard) {
+      lastFocusedCard.focus();
+      setLastFocusedCard(null);
+    }
+  }, [lastFocusedCard]);
+
+  const announceGameLose = (id, e) => {
+    setResult({
+      highScore:
+        memory.length > result.highScore ? memory.length : result.highScore,
+      info: {
+        title: 'Game Over!',
+        score: `${memory.length}/${images.length}`,
+        src: images.find((img) => img.id === id)?.src,
+        message: `You have clicked on this image twice!`,
+        lastPlayedCardElement: e.currentTarget,
+      },
+    });
+  };
+
+  const announceGameWin = (id, e) => {
+    setMemory([...memory, id]);
+    setResult({
+      highScore: images.length,
+      info: {
+        title: 'Congratulations!',
+        score: `${memory.length + 1}/${images.length}`,
+        message: `You have achieved the maximum score!`,
+        lastPlayedCardElement: e.currentTarget,
+      },
+    });
+  };
+
+  const keepPlaying = (id) => setMemory([...memory, id]);
+
+  const handleCardClicked = (id, e) => {
+    setImages(shuffleArray(images));
+    if (memory.includes(id)) announceGameLose(id, e);
+    else if (memory.length + 1 === images.length) announceGameWin(id, e);
+    else keepPlaying(id, e);
+  };
+
+  const handleModalClose = () => {
+    setLastFocusedCard(result.info.lastPlayedCardElement);
+    setResult({ ...result, info: null });
+    setMemory([]);
+  };
+
   return (
     <>
-      <h1 className="title">Odin Memory Card</h1>
-      <Game />
+      <div className="header">
+        <h1 className="title">Odin Memory Card</h1>
+        {Array.isArray(images) && (
+          <Info
+            score={memory.length}
+            maxScore={images.length}
+            highScore={result.highScore}
+          />
+        )}
+      </div>
+      <Board cardImages={images} onCardClicked={handleCardClicked} />
+      {result.info && (
+        <ResultModal info={result.info} onClose={handleModalClose} />
+      )}
     </>
   );
 }
--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,17 +1,79 @@
+import { useEffect, useRef } from 'react';
 import extractAltFromSrc from '../utils/extractAltFromSrc';
-import Modal from './Modal';
 
 function ResultModal({ info, onClose }) {
+  const modalBodyRef = useRef(null);
+  const closeBtn = useRef(null);
+
+  useEffect(() => {
+    closeBtn.current?.focus();
+    setTimeout(() => {
+      modalBodyRef.current?.scrollIntoView({
+        behavior: 'instant',
+        inline: 'center',
+        block: 'center',
+      });
+    }, 100);
+  }, []);
+
+  useEffect(() => {
+    const trapFocus = (e) => {
+      // https://developer.mozilla.org/en-US/docs/Web/API/Node/compareDocumentPosition
+      const isDescendant = Boolean(
+        modalBodyRef.current.compareDocumentPosition(e.target) &
+          Node.DOCUMENT_POSITION_CONTAINED_BY
+      );
+      if (modalBodyRef.current && !isDescendant) {
+        modalBodyRef.current.focus();
+      }
+    };
+    document.addEventListener('focusin', trapFocus);
+    document.documentElement.setAttribute('style', 'position: relative;');
+    return () => {
+      document.removeEventListener('focusin', trapFocus);
+      document.documentElement.removeAttribute('style');
+    };
+  }, []);
+
+  const handleEscapeKeyPress = (e) => {
+    if (e.key === 'Escape') {
+      e.preventDefault();
+      onClose();
+    }
+  };
+
   return (
-    <Modal className="modal" onClose={onClose}>
-      <h2 className="modal-title">{info.title}</h2>
-      <p className="modal-score">Your score is {info.score}</p>
-      {info.src && <img src={info.src} alt={extractAltFromSrc(info.src)} />}
-      <p className="modal-body">{info.message}</p>
-      <button autoFocus type="button" className="modal-btn" onClick={onClose}>
-        OK!
-      </button>
-    </Modal>
+    <div className="result-modal" onClick={onClose}>
+      <div
+        tabIndex={-1}
+        ref={modalBodyRef}
+        className="modal-body"
+        onClick={(e) => e.stopPropagation()}
+        onKeyDownCapture={handleEscapeKeyPress}
+        onFocus={(e) => {
+          if (e.target === e.currentTarget) {
+            e.target.scrollIntoView({
+              behavior: 'instant',
+              inline: 'center',
+              block: 'center',
+            });
+          }
+        }}
+      >
+        <h2 className="modal-title">{info.title}</h2>
+        <p className="modal-score">Your score is {info.score}</p>
+        {info.src && <img src={info.src} alt={extractAltFromSrc(info.src)} />}
+        <p className="modal-message">{info.message}</p>
+        <button
+          type="button"
+          className="modal-btn"
+          onClick={onClose}
+          ref={closeBtn}
+        >
+          OK!
+        </button>
+      </div>
+    </div>
   );
 }
 
